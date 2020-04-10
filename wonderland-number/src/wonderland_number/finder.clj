fa674d93d5e96adfd54c2cb5e51d345cd8236fc2
AnhQuan Nguyen <j4qfrost@gmail.com>
2020-04-10 04:10:31 -0700

    finder

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,19 @@
 (ns wonderland-number.finder)
 
+(def ^:const multis (range 2 7))
+
+(defn digitset [n]
+  (-> n str set))
+
+(defn multiples [n]
+  (for [m multis] (* n m)))
+
+(defn check [n]
+  (let [dign (digitset n)
+        digms (for [m (multiples n)] (digitset m))]
+    (every? #(= % dign) digms)))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (let [upper (int (/ 1000000 6))]
+    (loop [n upper]
+      (if (check n) n (recur (- n 1))))))
